562c6f860a5155d8456d32f2014a145c4ccd60ab
Kate Bishop <itskatebishop@gmail.com>
2023-10-16 22:28:41 -0400

    initial commit, readme

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+var cors = require('cors')
+const bodyParser = require('body-parser');
+
+const DataAccessObject = require('./dataAccessObject');
+const Comment = require('./comment');
+
+const app = express();
+const port = process.env.PORT || 3001;
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const dataAccessObject = new DataAccessObject('./database.sqlite3');
+const comment = new Comment(dataAccessObject);
+
+comment.createTable().catch(error => {
+  console.log(`Error: ${JSON.stringify(error)}`);
+});
+
+app.post('/createComment', cors(corsOptions), function(request, response) {
+  const { body } = request;
+  comment.createComment(body).then(result => {
+    response.send(result);
+  });
+});
+
+app.get('/getComment', function(request, response) {
+  const { body } = request;
+  const { id } = body;
+  comment.getComment(id).then(result => {
+    response.send(result);
+  });
+});
+
+app.get('/getComments', cors(corsOptions), function(request, response) {
+  comment.getComments().then(result => {
+    response.send(result);
+  });
+});
+
+app.delete('/deleteComments', function(request, response) {
+  comment.deleteComments().then(result => {
+    response.send(result);
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  const rootDir = __dirname.replace('/server', '');
+  response.sendFile(`${rootDir}/src/index.html`);
+});
